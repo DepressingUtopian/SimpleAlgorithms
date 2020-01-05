e441c2b934349f5450072dba8da7e95a025943b5
DepressingUtopian <DepressingUtopian@gmail.com>
2020-01-05 22:18:43 +0700

    Добавьте файлы проекта.

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SimpleAlgorithms
+{
+    public class BinarySearch
+    {
+        public static uint Find(int item,int[] array)
+        {
+            uint min = 0;
+            uint max =  (uint)array.Length - 1;
+            uint mid = 0;
+
+            while (min <= max)
+            {
+                mid = (min + max) / 2;
+                if (item == array[mid])
+                    return mid;
+                else if (item > array[mid])
+                {
+                    min = mid + 1;
+                }
+                else if (item < array[mid])
+                {
+                    max = mid - 1;
+                }
+            }
+            throw new Exception("Элемент не найдет!");
+
+        }
+        public static void RandomizeArray(ref int[] array,int N)
+        {
+            if (N <= 0)
+                throw new Exception("Задан не верный размер массива!");
+            
+            Random random = new Random();
+            //Инициализация массива
+            array = new int[random.Next(0, N)];
+
+            //Заполнение массива
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next(0, Int32.MaxValue);
+            //Сортировка массива
+            Array.Sort<int>(array);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+}
---
+++ SimpleAlgorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[0];
+            BinarySearch.RandomizeArray(ref array,100000000);
+            BinarySearch.Find(array[3], array);
+        }
+    }
+}
