7f578379cc397bcf030f0f0d10471c9d96c8e19f
DepressingUtopian <DepressingUtopian@gmail.com>
2020-07-30 21:40:07 -0700

    Add breadth-first search algorithm - Удалил класс HashTable. - Добавил класс реализующий поиск в ширину.

---
+++ BFS/BFS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleAlgorithms
+{
+    public class BFS
+    {
+        public static bool Find(int [,] graph, int idx_start_node, int idx_goal_node)
+        {
+            Queue<int> search_queue = new Queue<int>();
+            Queue<int> searched = new Queue<int>();
+     
+            AddConnectedVertices(graph, idx_start_node,ref search_queue);
+
+            while (search_queue.Count > 0)
+            {
+                int nodeNumber = search_queue.Dequeue();
+                if (!searched.Contains(nodeNumber))
+                {
+                    if (nodeNumber == idx_goal_node)
+                        return true;
+                    else
+                    {
+                        AddConnectedVertices(graph, nodeNumber, ref search_queue);
+                        searched.Enqueue(nodeNumber);
+                    }
+                }
+            }
+            return false;
+        }
+        
+        private static void AddConnectedVertices(int[,] graph, int idx_node, ref Queue<int> queue)
+        {
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (i == idx_node)
+                    continue;
+                if (graph[idx_node, i] != 0)
+                    queue.Enqueue(i);
+            }
+        }
+    }
+}
--- SimpleAlgorithms/Program.cs
+++ SimpleAlgorithms/Program.cs
@@ -13,11 +13,12 @@ namespace SimpleAlgorithms
             //BinarySearch.RandomizeArray(ref array,100000000,true);
            // BinarySearch.Find(array[3], array);
 
-            BinarySearch.RandomizeList(ref list, 10000, false);
+            DataSet.DataSet.RandomizeList(ref list, 10000, false);
             SelectionSort.Sort(list,out sortedList);
-            BinarySearch.RandomizeList(ref list, 10000, false);
+            DataSet.DataSet.RandomizeList(ref list, 10000, false);
             QuickSort.Sort(list);
-            DataSet.DataSet.GenMatrixGraph(6,10);
+            var graph =  DataSet.DataSet.GenMatrixGraph(6,10);
+            BFS.Find(graph,1,5);
         }
     }
 }
